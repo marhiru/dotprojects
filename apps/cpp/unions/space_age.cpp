041f516ba183dcfdbba75592f63cb5703c522dd0
marhiru <marcosgamern@gmail.com>
2025-11-14 23:57:00 -0300

    feat: checkpoint 02

--- apps/cpp/unions/space_age.cpp
+++ apps/cpp/unions/space_age.cpp
@@ -1,33 +1,30 @@
-#include <cstdio>
+#include <array>
+#include <string>
 
-class PlanetsDelta {
+class Planets {
 public:
-  const double earth_period = 365.2425; // One year in earth days equivalent;
-  // Orbits delta;
-  const float mercury = 0.2408467 * earth_period;
-  const float venus = 0.61519726 * earth_period;
-  const float earth = 1.0 * earth_period;
-  const float mars = 1.8808158 * earth_period;
-  const float jupiter = 11.862615 * earth_period;
-  const float saturn = 29.447498 * earth_period;
-  const float uranus = 84.016846 * earth_period;
-  const float neptune = 164.79132 * earth_period;
-  ;
+  std::string planet;
+  float period;
+
+  Planets() : planet(""), period(0.0) {}
+
+  Planets(const std::string &p, float d) : planet(p), period(d) {}
 };
 
-enum Planets { MERCURY, VENUS, EARTH, MARS, JUPITER, SATURN, URANUS, NEPTUNE };
-
-long long PeriodInSeconds(float days) {
-  long long seconds = static_cast<long long>(days) * 24 * 60 * 60;
-
-  return 0;
-}
-
 int main() {
-  Planets planets;
-  PlanetsDelta delta;
-  std::printf("%f", delta.mars);
-  int age;
+  const double earth_period = 365.2425;
+  std::array<Planets, 8> planets = {
+      Planets("mercury", 0.2408467), Planets("venus", 0.61519726),
+      Planets("earth", 1.0),         Planets("mars", 1.8808158),
+      Planets("jupiter", 11.862615), Planets("saturn", 29.447498),
+      Planets("uranus", 84.016846),  Planets("neptune", 164.79132),
+  };
+
+  int i = 0;
+  for (const auto &p : planets) {
+    std::printf("%s: ", p.planet.c_str());
+    std::printf("%f\n", p.period);
+  }
 
   return 0;
 }
