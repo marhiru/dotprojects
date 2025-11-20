a2e14d20e3ff125c792d132fddb5acc9b1073d16
marhiru <marcosgamern@gmail.com>
2025-11-20 14:54:38 -0300

    feat: added algorithm

---
+++ apps/cpp/algorithms/zero-one.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+/*
+ * We need are making some algorithm and we need this fields below:
+ * (n) = Operations number count O(n)
+ * (r) = Final result of algorithm
+ * (s) = Initial value (source of truth)
+ *
+ * Rules about this challenge:
+ * (1) = You cannot use any STL resource (Standard library)
+ * (2) = Simplicity above all: Use modern c++ concepts while writing simple code
+ * (3) =
+ */
+
+class Main {
+public:
+  int n, r, s;
+};
+
+int main() {
+  Main MyClass;
+
+  for (int i = 0; i < 8; i++) {
+    std::cout << i;
+  }
+  return 0;
+}
