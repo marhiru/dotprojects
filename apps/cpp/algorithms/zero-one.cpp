5d3a5e6f24d69d28435c94703c616cb35dcb0699
marhiru <marcosgamern@gmail.com>
2025-11-22 23:24:54 -0300

    fix: finished algorithm

--- apps/cpp/algorithms/zero-one.cpp
+++ apps/cpp/algorithms/zero-one.cpp
@@ -1,28 +1,26 @@
 #include <iostream>
 
-/*
- * We need are making some algorithm and we need this fields below:
- * (n) = Operations number count O(n)
- * (r) = Final result of algorithm
- * (s) = Initial value (source of truth)
- *
- * Rules about this challenge:
- * (1) = You cannot use any STL resource (Standard library)
- * (2) = Simplicity above all: Use modern c++ concepts while writing simple code
- * (3) =
- */
-
-class Main {
-public:
-  int n, r, s;
-};
-
 int main() {
-  Main MyClass;
+  int c{10};
 
-  for (int i = 0; i < 8; i++) {
-    for (int v{0}; v < i; v++) {
-    std::cout << i;
+  for (int i{0}; i < 10; ++i) {
+    // Left spaces
+    for (int j{0}; j < i; j++) {
+      std::cout << " ";
+    }
+
+    std::cout << c;
+
+    // Right numbers
+    for (int p{10}; p > i; p--) {
+      std::cout << " " << " ";
+    }
+
+    std::cout << c;
+    std::cout << "\n";
+
+    c += 1;
   }
+
   return 0;
 }
