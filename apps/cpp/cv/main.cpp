93811ae86902790afc5a6c441230c510fb086eeb
marhiru <marcosgamern@gmail.com>
2025-11-30 09:10:33 -0300

    feat: added minimal main file

--- apps/cpp/cv/main.cpp
+++ apps/cpp/cv/main.cpp
@@ -1,6 +1,15 @@
-#include <cstdio>
+#include <iostream>
+#include <opencv4/opencv2/opencv.hpp>
 
-int main() {
-  std::printf("teste");
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <image_path>\n";
+    return -1;
+  }
+
+  cv::Mat image = cv::imread(argv[0], cv::IMREAD_COLOR);
+
+  if (image.empty())
+    return -1;
   return 0;
 }
