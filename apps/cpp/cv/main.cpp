bf3bd3eee446e6193ff38aa9692318ee04f689b7
marhiru <marcosgamern@gmail.com>
2025-12-04 17:01:34 -0300

    feat: initializating open-cv project in cv

--- apps/cpp/cv/main.cpp
+++ apps/cpp/cv/main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/core/types.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio.hpp>
 #include <opencv4/opencv2/opencv.hpp>
 
 int main(int argc, char **argv) {
@@ -7,9 +12,31 @@ int main(int argc, char **argv) {
     return -1;
   }
 
-  cv::Mat image = cv::imread(argv[0], cv::IMREAD_COLOR);
+  cv::VideoCapture cap;
 
-  if (image.empty())
-    return -1;
+  if (argc > 1) {
+    std::string path = argv[1];
+    cap.open(path);
+
+    if (cap.isOpened()) {
+      cv::Mat image = cv::imread(path, cv::IMREAD_COLOR);
+
+      if (image.empty()) {
+        std::cerr << "Error: Could not open file: " << path << "\n";
+        return -1;
+      }
+
+      cv::Mat edges;
+      cv::cvtColor(image, edges, cv::COLOR_BGR2GRAY);
+      cv::Canny(edges, edges, 50, 150);
+
+      cv::Mat side_by_side;
+      cv::hconcat(image, cv::Mat(image.size(), image.type(), cv::Scalar(0)),
+                  side_by_side);
+      cv::Mat right;
+      cv::hconcat(side_by_side, right, side_by_side);
+    }
+  }
+
   return 0;
 }
