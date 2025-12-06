31b733dbb54ab95c8007b7f09f2fb6aa981219e9
marhiru <marcosgamern@gmail.com>
2025-12-05 23:45:43 -0300

    feat: added support to more types like: string_view are supported

--- apps/cpp/unitemu/mio.h
+++ apps/cpp/unitemu/mio.h
@@ -1,41 +1,21 @@
-
-#ifndef MIO_H
-
-#define MIO_H
 #include <cstdio>
-#include <format>
-#include <iostream>
-#include <iterator>
 #include <ostream>
 #include <string_view>
-#include <type_traits>
+#include <utility>
 
-namespace mio {
+#ifndef MIO_H
+#define MIO_H
 
-template <typename Char> class basic_printf_context {
-private:
-  basic_appender<Char> out_;
-  basic_format_args<basic_printf_content> args_;
-
-  static_assert(std::is_same<Char, char>::value ||
-                    std::is_same<Char, wchar_t>::value,
-                "Unsupported code unit type.");
-
-public:
-  using char_type = Char;
-};
-
-template <typename Char> struct vprintf_args {
-  using type = std::basic_format_args<basic_printf_context<Char>>;
-};
-
-template <typename Char>
-auto vfprintf(std::FILE *__restrict f, std::basic_string_view<Char> fmt,
-              typename vprintf_args<Char>::type args) -> int {
-  auto buf = basic_memory_buffer<Char>();
-  std::vfprintf(buf, fmt, args);
-};
-
-} // namespace mio
+template <typename... T> int println(const char *fmt, T &&...arg) {
+  if constexpr (sizeof...(T) != 0) {
+    const int n = std::printf(fmt, std::forward<T>(arg)...);
+    std::putchar('\n');
+    return n + 1;
+  }
+  return std::fputs(fmt, stdout) + std::putchar('\n');
+}
+inline int println(std::string_view sv) {
+  return std::fputs(sv.data(), stdout) + std::putchar('\n');
+}
 
 #endif // MIO_H
