98ed9af20bec62828db6ee9648bc0e4ac1468cc7
marhiru <marcosgamern@gmail.com>
2025-12-05 23:44:12 -0300

    fix: finished testing

--- apps/cpp/unitemu/main.cpp
+++ apps/cpp/unitemu/main.cpp
@@ -1,10 +1,7 @@
 #include "mio.h"
-#include <cstdio>
 
 int main() {
-  using mio::println;
-  std::string teste{"string"};
-  println("Teste IO {%s} library com o println", teste);
-
+  std::string tes{"askdj"};
+  println(tes);
   return 0;
 }
